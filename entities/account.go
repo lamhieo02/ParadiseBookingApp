52db286683095c1b249ee196e4fcdaf01bb61d78
Lam Nguyen <lamlklk2002@gmail.com>
2023-11-24 23:38:44 +0700

    update

--- entities/account.go
+++ entities/account.go
@@ -6,16 +6,17 @@ import (
 
 type Account struct {
 	common.SQLModel
-	Username string `json:"username" gorm:"column:username"`
-	Email    string `json:"email" gorm:"column:email"`
-	FullName string `json:"full_name" gorm:"column:full_name"`
-	Role     int    `json:"role" gorm:"role"`
-	Status   int    `json:"status" gorm:"column:status"`
-	Password string `json:"password" gorm:"column:password"`
-	Address  string `json:"address" gorm:"column:address"`
-	Phone    string `json:"phone" gorm:"column:phone"`
-	Dob      string `json:"dob" gorm:"column:Dob"`
-	Avatar   string `json:"avatar" gorm:"avatar"`
+	Username        string `json:"username" gorm:"column:username"`
+	Email           string `json:"email" gorm:"column:email"`
+	FullName        string `json:"full_name" gorm:"column:full_name"`
+	Role            int    `json:"role" gorm:"role"`
+	Status          int    `json:"status" gorm:"column:status"`
+	Password        string `json:"password" gorm:"column:password"`
+	Address         string `json:"address" gorm:"column:address"`
+	Phone           string `json:"phone" gorm:"column:phone"`
+	Dob             string `json:"dob" gorm:"column:Dob"`
+	Avatar          string `json:"avatar" gorm:"avatar"`
+	IsEmailVerified int    `json:"is_email_verified" gorm:"is_email_verified"`
 }
 
 func (Account) TableName() string {
--- modules/verify_emails/usecase/check_verify_code_is_matching.go
+++ modules/verify_emails/usecase/check_verify_code_is_matching.go
@@ -3,6 +3,7 @@ package verifyemailsusecase
 import (
 	"context"
 	"paradise-booking/common"
+	"paradise-booking/entities"
 
 	"gorm.io/gorm"
 )
@@ -20,6 +21,16 @@ func (uc *verifyEmailsUseCase) CheckVerifyCodeIsMatching(ctx context.Context, em
 	// check if verify code is expired
 	if data.IsExpired() {
 		return common.ErrExpiredVerifyCode("verify code", err)
+	}
+
+	// if all is ok => update status to verified
+	account := &entities.Account{
+		IsEmailVerified: 1,
+	}
+
+	err = uc.accountStore.UpdateIsVerifyEmailByEmail(ctx, account.Email)
+	if err != nil {
+		return common.ErrCannotUpdateEntity("account", err)
 	}
 
 	return nil
