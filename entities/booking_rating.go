91c3e48ffedf2f61b2bff65fd55ecd491264a2fa
Lam Nguyen <lamlklk2002@gmail.com>
2023-12-09 01:36:16 +0700

    update booking make comment

--- entities/booking_rating.go
+++ entities/booking_rating.go
@@ -6,6 +6,7 @@ type BookingRating struct {
 	common.SQLModel
 	UserId    int    `json:"user_id" gorm:"column:user_id"`
 	BookingId int    `json:"booking_id" gorm:"column:booking_id"`
+	PlaceId   int    `json:"place_id" gorm:"column:place_id"`
 	Title     string `json:"title" gorm:"column:title"`
 	Content   string `json:"content" gorm:"column:content"`
 	Rating    int    `json:"rating" gorm:"column:rating"`
--- modules/booking_rating/iomodel/create_place_rating_req.go
+++ modules/booking_rating/iomodel/create_place_rating_req.go
@@ -2,6 +2,7 @@ package iomodel
 
 type CreateBookingRatingReq struct {
 	BookingID int     `json:"booking_id"`
+	PlaceID   int     `json:"place_id"`
 	Title     string  `json:"title"`
 	Content   string  `json:"content"`
 	Rating    float64 `json:"rating"`
