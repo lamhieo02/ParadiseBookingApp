b02312c2490773f650ad6e7d29948e6d005393ab
Lam Nguyen <lamlklk2002@gmail.com>
2023-12-09 09:39:34 +0700

    add comment booking

--- modules/booking_rating/usecase/get_comment_by_booking_id.go
+++ modules/booking_rating/usecase/get_comment_by_booking_id.go
@@ -2,15 +2,31 @@ package bookingratingusecase
 
 import (
 	"context"
+	"log"
 	"paradise-booking/common"
 	"paradise-booking/entities"
+	"paradise-booking/modules/booking_rating/iomodel"
 )
 
-func (uc *bookingRatingUsecase) GetCommentByBookingID(ctx context.Context, bookingID int) ([]entities.BookingRating, error) {
+func (uc *bookingRatingUsecase) GetCommentByBookingID(ctx context.Context, bookingID int) ([]iomodel.GetCommentResp, error) {
 	res, err := uc.BookingRatingSto.GetByCondition(ctx, map[string]interface{}{"booking_id": bookingID})
 	if err != nil {
 		return nil, common.ErrCannotGetEntity(entities.BookingRating{}.TableName(), err)
 	}
 
-	return res, nil
+	var result []iomodel.GetCommentResp
+
+	for _, bookingRate := range res {
+		user, err := uc.AccountSto.GetProfileByID(ctx, bookingRate.UserId)
+		if err != nil {
+			log.Printf("Error when get user profile by id: %v\n", err)
+			continue
+		}
+		result = append(result, iomodel.GetCommentResp{
+			DataRating: bookingRate,
+			DataUser:   *user,
+		})
+	}
+
+	return result, nil
 }
