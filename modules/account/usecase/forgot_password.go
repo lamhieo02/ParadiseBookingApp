9b5578bc7e9a6a81574088d2157d87b4999b783c
Lam Nguyen <lamlklk2002@gmail.com>
2023-12-24 11:13:47 +0700

    check email exist before reset pass

--- modules/account/usecase/forgot_password.go
+++ modules/account/usecase/forgot_password.go
@@ -2,13 +2,25 @@ package accountusecase
 
 import (
 	"context"
+	"errors"
+	"paradise-booking/common"
 	"paradise-booking/worker"
 	"time"
 
 	"github.com/hibiken/asynq"
+	"gorm.io/gorm"
 )
 
 func (uc *accountUseCase) ForgotPassword(ctx context.Context, email string) error {
+	// check email exist
+	_, err := uc.accountStorage.GetAccountByEmail(ctx, email)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return common.ErrEmailNotExist("account", errors.New("email not exist"))
+		}
+		return err
+	}
+
 	// send email
 
 	taskPayload := worker.PayloadSendVerifyResetCodePassword{
