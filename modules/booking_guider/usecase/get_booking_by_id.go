16247fbd196f2395b33ef76b59b4efe8d3ef8155
Lam Nguyen <lamlklk2002@gmail.com>
2024-05-15 23:15:50 +0700

    fix bug api get booking guider by id

--- modules/booking_guider/usecase/get_booking_by_id.go
+++ modules/booking_guider/usecase/get_booking_by_id.go
@@ -3,23 +3,24 @@ package bookingguiderusecase
 import (
 	"context"
 	bookingguiderconvert "paradise-booking/modules/booking_guider/convert"
+	bookingguideriomodel "paradise-booking/modules/booking_guider/iomodel"
 )
 
-func (uc *bookingGuiderUseCase) GetBookingByID(ctx context.Context, bookingGuiderID int) error {
+func (uc *bookingGuiderUseCase) GetBookingByID(ctx context.Context, bookingGuiderID int) (*bookingguideriomodel.GetBookingGuiderResp, error) {
 
 	data, err := uc.bookingGuiderSto.GetByID(ctx, bookingGuiderID)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	res := bookingguiderconvert.ConvertBookingEntityToModel(data)
 	calendar, err := uc.calendarSto.GetByID(ctx, data.CalendarGuiderID)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	res.CalendarGuider.DateFrom = *calendar.DateFrom
 	res.CalendarGuider.DateTo = *calendar.DateTo
 
-	return nil
+	return res, nil
 }
