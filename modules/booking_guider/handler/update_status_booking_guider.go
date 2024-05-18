0a9a794c206080fd78c69ff7aa9073cdf25b4379
Lam Nguyen <lamlklk2002@gmail.com>
2024-05-18 22:20:16 +0700

    api update

--- modules/booking_guider/handler/update_status_booking_guider.go
+++ modules/booking_guider/handler/update_status_booking_guider.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func (hdl *bookingGuiderHandler) UpdateStatusBookingGuider() gin.HandlerFunc {
+func (hdl *bookingGuiderHandler) ConfirmStatusBookingGuider() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		bookingID, _ := c.GetQuery("booking_guider_id")
 		status, _ := c.GetQuery("status")
@@ -23,5 +23,23 @@ func (hdl *bookingGuiderHandler) UpdateStatusBookingGuider() gin.HandlerFunc {
 		}
 
 		c.Redirect(http.StatusMovedPermanently, constant.UrlConfirmBookingSuccess)
+	}
+}
+
+func (hdl *bookingGuiderHandler) UpdateStatusBookingGuider() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		bookingID, _ := c.GetQuery("booking_guider_id")
+		status, _ := c.GetQuery("status")
+
+		bookingId, _ := strconv.Atoi(bookingID)
+		statusInt, _ := strconv.Atoi(status)
+
+		err := hdl.bookingGuiderUC.UpdateStatusBooking(c.Request.Context(), bookingId, statusInt)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"data": true})
 	}
 }
