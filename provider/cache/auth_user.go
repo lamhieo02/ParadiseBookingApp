646275e267a5d5ab112554d7ff07b290a3fd3635
Lam Nguyen <lamlklk2002@gmail.com>
2024-05-20 21:49:26 +0700

    fix redis

--- provider/cache/auth_user.go
+++ provider/cache/auth_user.go
@@ -33,7 +33,7 @@ func (c *authUserCache) GetProfileByID(ctx context.Context, id int) (*entities.A
 	}
 
 	// if data is found in cache, then return the data
-	if account != nil {
+	if account.Id != 0 {
 		return account, nil
 	}
 
@@ -63,7 +63,7 @@ func (c *authUserCache) GetAccountByEmail(ctx context.Context, email string) (*e
 	}
 
 	// if data is found in cache, then return the data
-	if account != nil {
+	if account.Id != 0 {
 		return account, nil
 	}
 
