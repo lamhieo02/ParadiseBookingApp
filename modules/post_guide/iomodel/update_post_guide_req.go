4c37c65c7c2d89644d073d5f1540ce03033093c4
Lam Nguyen <lamlklk2002@gmail.com>
2024-05-29 23:08:20 +0700

    add languages for update postguide

--- modules/post_guide/iomodel/update_post_guide_req.go
+++ modules/post_guide/iomodel/update_post_guide_req.go
@@ -1,12 +1,13 @@
 package postguideiomodel
 
 type UpdatePostGuideReq struct {
-	TopicID     int     `json:"topic_id" form:"topic_id"`
-	Title       string  `json:"title" form:"title"`
-	Description string  `json:"description" form:"description"`
-	Cover       string  `json:"cover" form:"cover"`
-	Lat         float64 `json:"lat" form:"lat"`
-	Lng         float64 `json:"lng" form:"lng"`
-	Address     string  `json:"address" form:"address"`
-	Schedule    string  `json:"schedule" form:"schedule"`
+	TopicID     int      `json:"topic_id" form:"topic_id"`
+	Title       string   `json:"title" form:"title"`
+	Description string   `json:"description" form:"description"`
+	Cover       string   `json:"cover" form:"cover"`
+	Lat         float64  `json:"lat" form:"lat"`
+	Lng         float64  `json:"lng" form:"lng"`
+	Address     string   `json:"address" form:"address"`
+	Languages   []string `json:"languages" form:"languages"`
+	Schedule    string   `json:"schedule" form:"schedule"`
 }
