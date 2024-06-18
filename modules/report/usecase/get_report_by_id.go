8a275f097b10b831416037b10bb9d38d9fef7371
Lam Nguyen <lamlklk2002@gmail.com>
2024-06-18 23:03:10 +0700

    add object value for report detail

--- modules/report/usecase/get_report_by_id.go
+++ modules/report/usecase/get_report_by_id.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"paradise-booking/constant"
 	"paradise-booking/entities"
+	"paradise-booking/modules/account/convert"
 	reportconvert "paradise-booking/modules/report/convert"
 	reportiomodel "paradise-booking/modules/report/iomodel"
 )
@@ -23,7 +24,71 @@ func (uc *reportUseCase) GetReportByID(ctx context.Context, id int) (*reportiomo
 
 	uc.getDataUser(result, account)
 
+	uc.getObjectValue(ctx, result)
 	return result, nil
+}
+
+func (uc *reportUseCase) getObjectValue(ctx context.Context, reportData *reportiomodel.GetReportResp) {
+	var objectVL interface{}
+	if reportData.ObjectType == constant.Report_Object_Type_Place {
+		place, err := uc.placeCache.GetPlaceByID(ctx, reportData.ObjectID)
+		if err != nil {
+			return
+		}
+
+		objectValue := reportiomodel.ObjectValue{}
+		objectValue.Title = place.Name
+		objectValue.Address = place.Address
+		objectValue.Cover = place.Cover
+		objectValue.Description = place.Description
+
+		objectVL = objectValue
+
+	} else if reportData.ObjectType == constant.Report_Object_Type_Tour {
+		postGuide, err := uc.postGuideCache.GetByID(ctx, reportData.ObjectID)
+		if err != nil {
+			return
+		}
+
+		objectValue := reportiomodel.ObjectValue{}
+		objectValue.Title = postGuide.Title
+		objectValue.Address = postGuide.Address
+		objectValue.Cover = postGuide.Cover
+		objectValue.Description = postGuide.Description
+
+		objectVL = objectValue
+
+	} else if reportData.ObjectType == constant.Report_Object_Type_Post_Review {
+		postReview, err := uc.postReviewSto.GetByID(ctx, reportData.ObjectID)
+		if err != nil {
+			return
+		}
+
+		objectValue := reportiomodel.ObjectValue{}
+		objectValue.Title = postReview.Title
+		objectValue.Description = "TOPIC" + constant.MapCategoryIDToName[postReview.Topic]
+		objectValue.Address = postReview.Country + " - " + postReview.State + " - " + postReview.District
+		objectValue.Cover = postReview.Image
+
+		objectVL = objectValue
+	} else if reportData.ObjectType == constant.Report_Object_Type_Comment {
+		comment, err := uc.commentSto.GetByID(ctx, reportData.ObjectID)
+		if err != nil {
+			return
+		}
+
+		objectVL = comment
+	} else if reportData.ObjectType == constant.Report_Object_Type_Guider || reportData.ObjectType == constant.Report_Object_Type_User ||
+		reportData.ObjectType == constant.Report_Object_Type_Vendor {
+		account, err := uc.accountCache.GetProfileByID(ctx, reportData.ObjectID)
+		if err != nil {
+			return
+		}
+
+		objectVL = convert.ConvertAccountEntityToInfoResp(account)
+	}
+
+	reportData.ObjectValue = objectVL
 }
 
 func (uc *reportUseCase) getDataUser(reportData *reportiomodel.GetReportResp, account *entities.Account) {
